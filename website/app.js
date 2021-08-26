b557b6fcc9c93fa6484b2bfd197267e70955b784
elpollodiablo87 <elpollodiablo3@gmail.com>
2021-08-26 17:01:02 +0200

    addedd the event listener, and a astatic prototype of the apicall

--- website/app.js
+++ website/app.js
@@ -1,12 +1,47 @@
 // Personal API Key for OpenWeatherMap API
-
-// Event listener to add function to existing HTML DOM element
-
-/* Function called by event listener */
+const key = 'a055dfbf06ba783119b20ef52f8bf6fb';
+// The API link
+const api = `api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${key}`;
 
 /* Function to GET Web API Data*/
+const gettingData = async () => {
+    await fetch('https://api.openweathermap.org/data/2.5/weather?zip=94040&appid=a055dfbf06ba783119b20ef52f8bf6fb').then((res) => {
+        return res.json();
+    }).then((data => {
+        const temperature = data.main.temp;
+        console.log(temperature);
+    })).catch((err) => {
+        console.log('Error: ', err);
+    })
+}
 
-/* Function to POST data */
+
+// Event listener to add function to existing HTML DOM element
+document.getElementById('generate').addEventListener('click', async () => {
+    const zip = document.getElementById('zip').value;
+    const userMood = document.getElementById('feelings').value;
+    gettingData().then(()=> {
+        const post = async (url = '/postData', data = {date: 1999, temperature, userMood}) => {
+            await fetch(url, {
+                method: 'POST', 
+                mode: 'cors', 
+                cache: 'no-cache',
+                credentials: 'same-origin',
+                headers: {
+                  'Content-Type': 'application/json'
+                },
+                redirect: 'follow',
+                referrerPolicy: 'no-referrer',
+                body: JSON.stringify(data)
+              })
+        }
+    }).then(()=> {
+        const getFinal = async () => {
+            await fetch ('/getData')
+        }
+    })
+   })
+    
 
 
-/* Function to GET Project Data */
+
